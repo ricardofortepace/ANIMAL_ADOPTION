dc2e16f56216f12ff72d9cf2fceaff00ec54b15c
Vinicius Miki <123morfar@gmail.com>
2022-03-04 11:38:09 -0300

    upload photo in animal form

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,5 +2,5 @@ class Animal < ApplicationRecord
   belongs_to :user
   has_many :adoptions, dependent: :destroy
   has_many_attached :photos
-  validates :specie, :genre, :age, presence: true
+  validates :specie, :genre, :age, :photos, presence: true
 end
