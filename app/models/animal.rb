f30d6be705395fa4cdce3a9c35479aa6df68cf00
Leonardo Reigota <leoreigotinha@gmail.com>
2022-03-08 10:56:23 -0300

    age from animal changed

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,7 +3,6 @@ class Animal < ApplicationRecord
   has_many :adoptions, dependent: :destroy
   has_many_attached :photos
   validates :specie, :genre, :age, :address, presence: true
-
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
